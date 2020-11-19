87ae4ee4f45b4e0edf1614ae7819a8e4b5fbaf5b
Fernando Alvarez <ferolsen@hotmail.com>
2020-11-19 16:10:54 -0600

    BackTracking v1
    Se implementa una solucion con backTracking

--- BackTracking.cpp
+++ BackTracking.cpp
@@ -4,37 +4,45 @@ void BackTracking::execute(vector<Country*> pCountries, vector<string> pColorPal
 {
 	colorPallete = pColorPallete;
 	std::vector<Country*>::iterator firstVectorElement = pCountries.begin();
-	bruteForce(pCountries, NULL, firstVectorElement);
+	bruteForce(pCountries, firstVectorElement);
+	cout << "End";
 }
 
-void BackTracking::bruteForce(vector<Country*> pVector,Country * pTriggerBackTracking, std::vector<Country*>::iterator pFirstCountry) {
+void BackTracking::bruteForce(vector<Country*> pVector, std::vector<Country*>::iterator pFirstCountry) {
 	std::vector<Country*>::iterator currentIterator = pVector.begin();
 	if (currentIterator != pVector.end())
 	{
 		Country* currentCountry = *currentIterator;
-		for (const auto& color : colorPallete) 
+		for (const auto& color : colorPallete)
 		{
-			tryToPaint(currentCountry, color);//Aca tengo un problema de recursion
-			if (currentCountry->isColored())
-			{
-				bruteForce(vector<Country*>(next(currentIterator), pVector.end()), pTriggerBackTracking, pFirstCountry);
+			if (!endOfAlgorithm) {
+				tryToPaint(currentCountry, color);//Aca tengo un problema de recursion
+				if (currentCountry->isColored())
+				{
+					bruteForce(vector<Country*>(next(currentIterator), pVector.end()), pFirstCountry);
+				}
 			}
 		}
-		if (!currentCountry->isColored()) {
-			pTriggerBackTracking = currentCountry;
+		if (!currentCountry->isColored() && triggeredBackTracking == NULL) {
+			triggeredBackTracking = currentCountry;
 		}
-		if (*pVector.begin() == *pFirstCountry && pTriggerBackTracking != NULL) {
-			cout<<pTriggerBackTracking->getId()<<endl;
-			pVector.erase(std::remove(pVector.begin(), pVector.end(), pTriggerBackTracking));
-			pTriggerBackTracking = NULL;													
-			bruteForce(pVector,pTriggerBackTracking,pFirstCountry);
+		if (*pVector.begin() == *pFirstCountry && triggeredBackTracking != NULL) {
+			cout << triggeredBackTracking->getId();
+			cout << "Llega al primero";
+			Country* current2 = *pFirstCountry;
+			cout << current2->getId();
+			pVector.erase(std::remove(pVector.begin(), pVector.end(), triggeredBackTracking));//Aca
+			cout << pVector.size();
+			triggeredBackTracking = NULL;
+			bruteForce(pVector, pFirstCountry);
 		}
 	}
+	else
+		endOfAlgorithm = true;
 }
 
 void BackTracking::tryToPaint(Country* pCountry, string color)//Podia ser un metodo que tuviera el country, pero solo va a ser usado aca
 {
-	pCountry->unPaint();
 	bool paint = true;
 	for (auto pair : pCountry->getNeighbors()) {
 		if (pair.second->getColor() == color) {
@@ -44,4 +52,6 @@ void BackTracking::tryToPaint(Country* pCountry, string color)//Podia ser un met
 	}
 	if (paint)
 		pCountry->setColor(color);
+	else 
+		pCountry->unPaint();
 }
--- BackTracking.h
+++ BackTracking.h
@@ -5,8 +5,10 @@ class BackTracking:public Strategy {
 
 	int index;
 	vector<string> colorPallete;
+	Country* triggeredBackTracking;// = null
+	bool endOfAlgorithm = false;
 
 	void execute(vector<Country*>pCountries, vector<string> pColorPallete);
-	void bruteForce(vector<Country*> pVector, Country* pTriggerBackTracking, std::vector<Country*>::iterator pFirstCountry);
+	void bruteForce(vector<Country*> pVector, std::vector<Country*>::iterator pFirstCountry);
 	void tryToPaint(Country* pCountry,string color);
 };
